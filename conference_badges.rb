b23c9979d043469c57f5f780ca750c2d776e706f
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-03-16 21:34:20 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,10 +16,8 @@ end
 def assign_rooms(attendees)
   room_assignments = Array.new
   attendees.each_with_index do |name, room|
-    attendees[name] = room
+    attendees[name.to_i].to_i = room.to_i
     badges.push
   end
   return attendees
 end
-
-assign_rooms(["Bobby","Jeff","Dean"])
