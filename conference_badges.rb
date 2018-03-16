4c89326ffb3301e6ea8a025c65691f13a3ccc444
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-03-16 17:43:51 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,7 +6,7 @@ end
 def badge_badge_creator(attendees)
   badges = Array.new
   attendees.each do |name|
-    badges.push('Hello, my name is #{name}.'')
+    badges.push('Hello, my name is #{name}.')
   end
   return badges
 end
