99472647a1f6a367499a9cb1a2438caf18e7f9e4
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-03-16 22:04:04 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,10 +16,18 @@ end
 def assign_rooms(attendees)
   room_assignments = Array.new
   attendees.each_with_index do |name, room|
+    attendees[name] = room
+    badges.push
+  end
+  return attendees
+end
+
+
+def assign_rooms(attendees)
+  room_assignments = Array.new
+  attendees.each_with_index do |name, room|
     room = room + 1
     room_assignments.push("Hello, #{name}! You'll be assigned to room #{room.to_s}!")
   end
   return room_assignments
 end
-
-assign_rooms(["clark","bruce","diana","barry"])
